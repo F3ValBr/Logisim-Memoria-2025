5918c21ba497eda751878b97e83d4cbceecec576
Felipe Valdebenito Bravo <felipe.vb1806@gmail.com>
2026-02-12 21:12:01 -0300

    release(3.0.3): logisimCL v3.0.3 available

--- src/com/cburch/logisim/Main.java
+++ src/com/cburch/logisim/Main.java
@@ -24,9 +24,9 @@ package com.cburch.logisim;
 import com.cburch.logisim.gui.start.Startup;
 
 public class Main {
-	public static final LogisimVersion VERSION = LogisimVersion.get(3, 0, 2);
+	public static final LogisimVersion VERSION = LogisimVersion.get(3, 0, 3);
 	public static final String VERSION_NAME = VERSION.toString();
-	public static final int COPYRIGHT_YEAR = 2025;
+	public static final int COPYRIGHT_YEAR = 2026;
 
 	public static void main(String[] args) {
 		Startup startup = Startup.parseArgs(args);
